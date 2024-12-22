e77fc3211ec4b3babd89b5d1f416d97662e8172d
pipiland2612 <nguyen.t.dang.minh@gmail.com>
2024-12-22 22:36:29 +0200

    Fix style

--- Algorithm Part I/Queue/src/Permutation.java
+++ Algorithm Part I/Queue/src/Permutation.java
@@ -2,14 +2,14 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
-   public static void main(String[] args){
+    public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<>();
-        while (!StdIn.isEmpty()){
+        while (!StdIn.isEmpty()) {
             queue.enqueue(StdIn.readString());
         }
-       for (int i = 0; i < k; i++) {
-           StdOut.println(queue.iterator().next());
-       }
-   }
+        for (int i = 0; i < k; i++) {
+            StdOut.println(queue.iterator().next());
+        }
+    }
 }
--- Algorithm Part I/Queue/src/RandomizedQueue.java
+++ Algorithm Part I/Queue/src/RandomizedQueue.java
@@ -1,166 +1,117 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private Node<Item> head, tail;
-    private int size;
-    // construct an empty randomized queue
+    private Item[] array;
+    private int lastIndex;
+
     public RandomizedQueue() {
-        head = null;
-        tail = null;
-        size = 0;
+        @SuppressWarnings("unchecked")
+        Item[] a = (Item[]) new Object[1];
+        array = a;
+        lastIndex = -1;
     }
 
-    // unit testing (required)
     public static void main(String[] args) {
+
     }
 
-    // is the randomized queue empty?
     public boolean isEmpty() {
-        return size == 0;
+        return size() == 0;
     }
 
-    // return the number of items on the randomized queue
     public int size() {
-        return size;
+        return lastIndex + 1;
     }
 
-    // add the item
     public void enqueue(Item item) {
-        if (item == null) throw new IllegalArgumentException("Item cannot be null");
-        Node<Item> newNode = new Node<>(item);
-        if (size == 0) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
+        if (item == null) {
+            throw new NullPointerException("Element e cannot be null.");
         }
-        size++;
+        // array is full
+        if (lastIndex + 1 == array.length) {
+            resize(array.length * 2);
+        }
+        array[++lastIndex] = item;
     }
 
-    // remove and return a random item
     public Item dequeue() {
-        if (isEmpty()) throw new NoSuchElementException("Queue is empty");
-        int randomIndex = (int) (Math.random() * size);
-        Node<Item> current = head;
-        for (int i = 0; i < randomIndex; i++) {
-            current = current.next;
+        if (isEmpty()) {
+            throw new NoSuchElementException("RandomizedQueue is empty.");
         }
-        return removeNode(current);
+        int i = StdRandom.uniformInt(lastIndex + 1);
+        Item removed = array[i];
+        array[i] = array[lastIndex];
+        array[lastIndex--] = null;
+        // resize array if it is only 25% full
+        if (size() > 0 && size() == array.length / 4) {
+            resize(array.length / 2);
+        }
+        return removed;
     }
 
-    // return a random item (but do not remove it)
     public Item sample() {
-        if (isEmpty()) throw new NoSuchElementException("Queue is empty");
-        int randomIndex = (int) (Math.random() * size);
-        Node<Item> current = head;
-        for (int i = 0; i < randomIndex; i++) {
-            current = current.next;
+        if (isEmpty()) {
+            throw new NoSuchElementException("RandomizedQueue is empty.");
         }
-        return current.item;
+        Item sample = null;
+        while (sample == null) {
+            sample = array[StdRandom.uniformInt(lastIndex + 1)];
+        }
+        return sample;
     }
 
-    // Helper method to remove a node from the linked list
-    private Item removeNode(Node<Item> node) {
-        if (node == head) {
-            return removeFirst();
-        } else if (node == tail) {
-            return removeLast();
-        } else {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            Item item = node.item;
-            node.next = null;
-            node.prev = null;
-            size--;
+    private void resize(int newCapacity) {
+        @SuppressWarnings("unchecked")
+        Item[] newArray = (Item[]) new Object[newCapacity];
+        int i = 0, j = 0;
+        while (i <= lastIndex) {
+            newArray[j++] = array[i++];
+        }
+        array = newArray;
+        lastIndex = j - 1;
+    }
+
+    public Iterator<Item> iterator() {
+        return new RandomizedIterator();
+    }
+
+    private class RandomizedIterator implements Iterator<Item> {
+
+        private final Item[] copiedArray;
+        private int copiedLastIndex;
+
+        RandomizedIterator() {
+            @SuppressWarnings("unchecked")
+            Item[] a = (Item[]) new Object[lastIndex + 1];
+            System.arraycopy(array, 0, a, 0, lastIndex + 1);
+            copiedArray = a;
+            copiedLastIndex = lastIndex;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return copiedLastIndex >= 0;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No more item.");
+            }
+            int i = StdRandom.uniformInt(copiedLastIndex + 1);
+            Item item = copiedArray[i];
+            copiedArray[i] = copiedArray[copiedLastIndex];
+            copiedArray[copiedLastIndex--] = null;
             return item;
         }
-    }
 
-    // Remove the first node and return its item
-    private Item removeFirst() {
-        if (isEmpty()) throw new NoSuchElementException("Queue is empty");
-        Node<Item> temp = head;
-        if (size == 1) {
-            head = null;
-            tail = null;
-        } else {
-            head = head.next;
-            head.prev = null;
-            temp.next = null;
-        }
-        size--;
-        return temp.item;
-    }
-
-    // Remove the last node and return its item
-    private Item removeLast() {
-        if (isEmpty()) throw new NoSuchElementException("Queue is empty");
-        Node<Item> temp = tail;
-        if (size == 1) {
-            head = null;
-            tail = null;
-        } else {
-            tail = tail.prev;
-            tail.next = null;
-            temp.prev = null;
-        }
-        size--;
-        return temp.item;
-    }
-
-    // return an independent iterator over items in random order
-    public Iterator<Item> iterator() {
-        return new RandomizedQueueIterator();
-    }
-
-    private static class Node<Item> {
-        Item item;
-        Node<Item> next, prev;
-
-        public Node(Item item) {
-            this.item = item;
-        }
-    }
-
-    private class RandomizedQueueIterator implements Iterator<Item> {
-        private final Node<Item>[] nodes;
-        private int currentIndex;
-
-        public RandomizedQueueIterator() {
-            nodes = new Node[size];
-            Node<Item> current = head;
-            for (int i = 0; i < size; i++) {
-                nodes[i] = current;
-                current = current.next;
-            }
-            shuffle(nodes);
-            currentIndex = 0;
-        }
-
-        private void shuffle(Node<Item>[] array) {
-            for (int i = array.length - 1; i > 0; i--) {
-                int randomIndex = (int) (Math.random() * (i + 1));
-                Node<Item> temp = array[i];
-                array[i] = array[randomIndex];
-                array[randomIndex] = temp;
-            }
-        }
-
-        public boolean hasNext() {
-            return currentIndex < nodes.length;
-        }
-
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException("No more elements");
-            return nodes[currentIndex++].item;
-        }
-
+        @Override
         public void remove() {
-            throw new UnsupportedOperationException("Remove not supported");
+            throw new UnsupportedOperationException("Remove unsupported.");
         }
     }
 }
