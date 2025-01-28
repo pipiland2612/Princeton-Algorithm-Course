335f9d3678abbbf0b38310beefad4d9e4e7e4bbb
pipiland2612 <nguyen.t.dang.minh@gmail.com>
2025-01-28 20:28:02 +0200

    Update and reset code

--- Algorithm Part II/untitled/src/Main.java
+++ Algorithm Part II/untitled/src/Main.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(generateSubset(new String[]{}));
+    }
+
+    public static List<List<String>> generateSubset(String[] strings) {
+        List<List<String>> results = new ArrayList<>();
+        subsetHelper(results, strings, new ArrayList<>(), 0);
+        return results;
+    }
+
+    private static void subsetHelper(List<List<String>> results, String[] strings, List<String> subset, int start) {
+        results.add(new ArrayList<>(subset));
+        for (int i = start; i < strings.length; i++) {
+            subset.add(strings[i]);
+            subsetHelper(results,strings, subset, i + 1);
+            subset.remove(subset.size() - 1);
+        }
+    }
 }
